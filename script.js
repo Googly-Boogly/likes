ad6899c317f1dc4ff79e07ceef463268835e9084
Googly-Boogly <102088081+Googly-Boogly@users.noreply.github.com>
2022-03-30 15:40:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var test = 1
+var test2 = 1
+var test3 = 1
+
+
+function like1() {
+    test++
+    var likess = document.querySelector('#likess')
+    likess.innerText = test
+}
+function like2() {
+    test2++
+    var likess = document.querySelector('#likes2')
+    likes2.innerText = test2
+}
+function like3() {
+    test3++
+    var likess = document.querySelector('#likes3')
+    likes3.innerText = test3
+}
